e54a4f90128c4d81bb4ce098e23690d9375f837c
Badlix2 <mathieu.leroux@etu.univ-amu.fr>
2022-12-18 10:18:30 +0100

    debut de la mise en place de l'envoi d'email

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function switchTo(sheet) {
     document.getElementById('pagestyle').setAttribute('href', sheet);
 }
 
+function sendEmail() {
+    var prenom = document.getElementById("prenom").value;
+    var nom = document.getElementById("nom").value;
+    var commentaire = document.getElementById("commentaire").value;
+    var notreAdresseMail = "alice.leroux@gmail.com";
+    // window.location.href("mailto:" + notreAdresseMail + '&subject=Avis de ' + prenom + ' ' + nom + '&body=' + emailBody);
+    window.open("mailto:alice.leroux@gmail.com");
+}
