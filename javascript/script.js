af38a32f1541267e312ffe2003bf160c498fd369
Badlix2 <mathieu.leroux@etu.univ-amu.fr>
2022-12-18 17:06:44 +0100

    delete repertorie js

--- javascript/script.js
+++
@@ -1,8 +0,0 @@
-function switchTo(sheet) {
-    document.getElementById('pagestyle').setAttribute('href', sheet);
-}
-
-// function getNote(form) {
-//     if (form.getElementById("score-5")).
-// }
-
