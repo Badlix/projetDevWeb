e8ec92f7c090593467f3dda508f843e7a7f0d8f2
Badlix2 <mathieu.leroux@etu.univ-amu.fr>
2022-12-18 15:15:25 +0100

    rajout de style spécifique au téléphone portable

--- javascript/script.js
+++ javascript/script.js
@@ -6,10 +6,3 @@ function switchTo(sheet) {
 //     if (form.getElementById("score-5")).
 // }
 
-function updateEmail(form) {
-    sujet = "Avis : " + form.getElementById("prenom") + " " + form.getElementById("nom");
-    texte = form.getElementById("commentaire");
-    complet = "mailto:alice.leroux@gmail.com?subject=" + sujet + "&body=" + texte;
-    console.log(complet);
-    document.getElementById("jsp").href = complet;
-}
